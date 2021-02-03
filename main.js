6f0ece2443101ac370c7a9741349415f9d0d2665
Harvey Lin <harveylin@cpp.edu>
2021-02-03 02:19:00 +0000

    Adding changes from replit

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
-clickValue = 1
-resources = new Map()
+clickValue = 1 // Amount of kills/exp per click
+clickValueMult = 1 // Multiplier for clickValue
+EXP_RATE = 100 // Multiplier for exp growth rate
+
+resources = new Map() // Amount of resources the player has
 resources["experience"] = 0
 resources["killStreak"] = 0
+resources["maxKillStreak"] = 0
+
+costs = new Map() // Cost of upgrades
+reqMaxStreak = new Map() // Required max kill streak for certain upgrades
+// TODO: Determine upgrades and costs
+// IDEA: Upgrade gun to improve clickValueMult (maybe they can stack?)
+// Hire party members to increase clickValue by fixed amount
+// M16: clickValue *= 3 (cuz 3 round burst lul)
+// AK: clickValue *= 5
+// Noobtube: clickValue *= 10
+
+// IDEA: Killstreaks unlock after max kill streak threshold
+// The higher the player's max kill streak, the more kill streaks they have access to
+// and can activate either sequentially or simultaneously
+// Might want to balance with a faster ticks_till_reset time as maxKillStreak increases
+// Give the better kill streaks longer cooldown
+// Harrier: Increase streak by 5 per tick for 5s-ish
+// Pavelow: Increase streak by 7 per tick for 10s-ish (maybe the harrier is upgraded to a pavelow via a shop upgrade or maybe they can stack)
+// Counter UAV: Stop nuisances from increasing for a set time
+// EMP: Can be longer duration version of counter UAV (again, maybe an upgrade or maybe can be stacked)
 
 tickRate = 16 // milliseconds
 timer = 0
-DRAW_RATE = 128
-TICKS_TILL_RESET = 300 / tickRate // Ticks until the killStreak is reset
-resetKillStreak = TICKS_TILL_RESET
+DRAW_RATE = 128 // Rate at which text on screen is updated
+ticks_till_reset = 3000 / tickRate // Ticks until the killStreak is reset (shorten as maxKillStreak increases?)
+resetKillStreak = ticks_till_reset
 
 var snd = new Audio("sounds/hitmarker.mp3");
 snd.volume = 0.25;
@@ -60,16 +83,17 @@ let baby = new Nuisance("baby",1,100,200)
 
 class Clicker { // The main clicker object
 
-  constructor(expTick) {
-    this.expTick = expTick
+  constructor() {
     this.canClick = true;
   }
 
   tick() {
     if (this.canClick) {
-      resources["experience"] += this.expTick
-      ++resources["killStreak"]
-      resetKillStreak = TICKS_TILL_RESET
+      resources["experience"] += clickValue * clickValueMult * EXP_RATE
+      resources["killStreak"] += clickValue * clickValueMult
+      if (resources["killStreak"] > resources["maxKillStreak"])
+        resources["maxKillStreak"] = resources["killStreak"]
+      resetKillStreak = ticks_till_reset
     }
   }
 }
@@ -78,12 +102,12 @@ function updateText() {
   // TODO: Update kill streak, experience, and nuisance counts
   for (var key in resources) {
     let element = document.getElementById(key)
-    element.innerHTML = resources[key]
+    element.innerHTML = resources[key].toFixed(0)
   }  
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  var clicker = new Clicker(clickValue)
+  var clicker = new Clicker()
 
   document.getElementById("clicker").onclick = function() {
     snd.currentTime = 0
@@ -94,9 +118,14 @@ document.addEventListener("DOMContentLoaded", function() {
   window.setInterval(function(){ // Clock function
     --resetKillStreak
     timer += tickRate
-    if (resetKillStreak <= 0) {
+    if (resetKillStreak <= 0 && resources["killStreak"] > 0) {
       resetKillStreak = 0
-      resources["killStreak"] = 0
+      resources["killStreak"] -= 0.5
+
+      if (resources["killStreak"] < 0)
+      {
+        resources["killStreak"] = 0
+      }
     }
     // Tick everything
     if (potty.isActive)
